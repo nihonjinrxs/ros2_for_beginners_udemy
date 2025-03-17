0ffb92a7c0eb44cb03cb186c61373025b9b42995
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2025-03-17 14:38:07 -0500

    Remove unneeded files from git repo

--- build/my_py_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/rharvey/code/ros2_for_beginners_2025/ros2_ws/install/my_py_pkg'
--- build/my_py_pkg/share/my_py_pkg/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/rharvey/code/ros2_for_beginners_2025/ros2_ws/build/my_py_pkg"
